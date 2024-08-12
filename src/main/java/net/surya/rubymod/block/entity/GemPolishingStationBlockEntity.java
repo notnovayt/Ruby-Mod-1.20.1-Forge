e9fb80f766cd7ab4b027a7e4db937049a2819ff0
notnovayt <gb8040@outlook.com>
2024-08-11 22:51:27 -0300

    Started working on the menu.

--- src/main/java/net/surya/rubymod/block/entity/GemPolishingStationBlockEntity.java
+++ src/main/java/net/surya/rubymod/block/entity/GemPolishingStationBlockEntity.java
@@ -11,6 +11,9 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
@@ -18,6 +21,7 @@ import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
+import net.surya.rubymod.item.ModItems;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -34,7 +38,7 @@ public class GemPolishingStationBlockEntity extends BlockEntity implements MenuP
     private int maxProgress = 78;
 
     public GemPolishingStationBlockEntity(BlockPos pPos, BlockState pBlockState) {
-        super(, pPos, pBlockState);
+        super(ModBlockEntities.GEM_POLISHING_BE.get(), pPos, pBlockState);
         this.data = new ContainerData() {
             @Override
             public int get(int pIndex) {
@@ -115,4 +119,52 @@ public class GemPolishingStationBlockEntity extends BlockEntity implements MenuP
         progress = pTag.getInt("gem_polishing_station.progress");
     }
 
+    public void tick(Level pLevel, BlockPos pPos, BlockState pState) {
+        if(hasRecipe()) {
+            increaseCraftingProgress();
+            setChanged(pLevel, pPos, pState);
+
+            if(hasProgressFinished()) {
+                craftItem();
+                resetProgress();
+            }
+        } else {
+            resetProgress();
+        }
+    }
+
+    private void resetProgress() {
+        progress = 0;
+    }
+
+    private void craftItem() {
+        ItemStack result = new ItemStack(ModItems.RUBY.get(), 1);
+        this.itemHandler.extractItem(INPUT_SLOT, 1, false);
+
+        this.itemHandler.setStackInSlot(OUTPUT_SLOT, new ItemStack(result.getItem(),
+                this.itemHandler.getStackInSlot(OUTPUT_SLOT).getCount() + result.getCount()));
+    }
+
+    private boolean hasRecipe() {
+        boolean hasCraftingItem = this.itemHandler.getStackInSlot(INPUT_SLOT).getItem() == ModItems.RAW_RUBY.get();
+        ItemStack result = new ItemStack(ModItems.RUBY.get());
+
+        return hasCraftingItem && canInsertAmountIntoOutputSlot(result.getCount()) && canInsertItemIntoOutputSlot(result.getItem());
+    }
+
+    private boolean canInsertItemIntoOutputSlot(Item item) {
+        return this.itemHandler.getStackInSlot(OUTPUT_SLOT).isEmpty() || this.itemHandler.getStackInSlot(OUTPUT_SLOT).is(item);
+    }
+
+    private boolean canInsertAmountIntoOutputSlot(int count) {
+        return this.itemHandler.getStackInSlot(OUTPUT_SLOT).getCount() + count <= this.itemHandler.getStackInSlot(OUTPUT_SLOT).getMaxStackSize();
+    }
+
+    private boolean hasProgressFinished() {
+        return progress >= maxProgress;
+    }
+
+    private void increaseCraftingProgress() {
+        progress++;
+    }
 }
--- src/main/java/net/surya/rubymod/block/entity/ModBlockEntities.java
+++ src/main/java/net/surya/rubymod/block/entity/ModBlockEntities.java
@@ -1,4 +1,24 @@
 package net.surya.rubymod.block.entity;
 
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.surya.rubymod.RubyMod;
+import net.surya.rubymod.block.ModBlocks;
+
 public class ModBlockEntities {
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
+            DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, RubyMod.MOD_ID);
+
+    public static final RegistryObject<BlockEntityType<GemPolishingStationBlockEntity>> GEM_POLISHING_BE =
+            BLOCK_ENTITIES.register("gem_polishing_be", () ->
+                    BlockEntityType.Builder.of(GemPolishingStationBlockEntity::new,
+                            ModBlocks.GEM_POLISHING_STATION.get()).build(null));
+
+
+    public static void register(IEventBus eventBus) {
+        BLOCK_ENTITIES.register(eventBus);
+    }
 }
