1c423b0a91e86f7a8ffa770e9fb7a5580ccc5252
notnovayt <gb8040@outlook.com>
2024-08-15 20:13:58 -0300

    Started working on the boat and chest boat classes.

---
+++ src/main/java/net/surya/rubymod/entity/custom/ModBoatEntity.java
@@ -0,0 +1,101 @@
+package net.surya.rubymod.entity.custom;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.util.ByIdMap;
+import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.surya.rubymod.block.ModBlocks;
+import net.surya.rubymod.item.ModItems;
+
+import java.util.function.IntFunction;
+
+public class ModBoatEntity extends Boat {
+    private static final EntityDataAccessor<Integer> DATA_ID_TYPE = SynchedEntityData.defineId(Boat.class, EntityDataSerializers.INT);
+
+    public ModBoatEntity(EntityType<? extends Boat> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public ModBoatEntity(Level level, double pX, double pY, double pZ) {
+        this(, level);
+        this.setPos(pX, pY, pZ);
+        this.xo = pX;
+        this.yo = pY;
+        this.zo = pZ;
+    }
+
+    @Override
+    public Item getDropItem() {
+        return switch (getModVariant()) {
+            case HAZELNUT -> ModItems.HAZELNUT_BOAT.get();
+        };
+    }
+
+    public void setVariant(Type pVariant) {
+        this.entityData.set(DATA_ID_TYPE, pVariant.ordinal());
+    }
+
+    public Type getModVariant() {
+        return Type.byId(this.entityData.get(DATA_ID_TYPE));
+    }
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(DATA_ID_TYPE, Type.HAZELNUT.ordinal());
+    }
+
+    protected void addAdditionalSaveData(CompoundTag pCompound) {
+        pCompound.putString("Type", this.getModVariant().getSerializedName());
+    }
+
+    protected void readAdditionalSaveData(CompoundTag pCompound) {
+        if (pCompound.contains("Type", 8)) {
+            this.setVariant(Type.byName(pCompound.getString("Type")));
+        }
+    }
+
+    public static enum Type implements StringRepresentable {
+        HAZELNUT(ModBlocks.HAZELNUT_PLANKS.get(), "hazelnut");
+
+        private final String name;
+        private final Block planks;
+        public static final StringRepresentable.EnumCodec<ModBoatEntity.Type> CODEC = StringRepresentable.fromEnum(ModBoatEntity.Type::values);
+        private static final IntFunction<Type> BY_ID = ByIdMap.continuous(Enum::ordinal, values(), ByIdMap.OutOfBoundsStrategy.ZERO);
+
+        private Type(Block pPlanks, String pName) {
+            this.name = pName;
+            this.planks = pPlanks;
+        }
+
+        public String getSerializedName() {
+            return this.name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public Block getPlanks() {
+            return this.planks;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+
+        public static ModBoatEntity.Type byId(int pId) {
+            return BY_ID.apply(pId);
+        }
+
+        public static ModBoatEntity.Type byName(String pName) {
+            return CODEC.byName(pName, HAZELNUT);
+        }
+    }
+}
